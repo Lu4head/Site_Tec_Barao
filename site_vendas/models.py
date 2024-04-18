619aa4040c947d87fc45ecbb209ec75467f6876c
GDuzzi <gustavotduzzi@outlook.com>
2024-04-18 13:18:22 -0300

    Teste conexão

--- site_vendas/models.py
+++ site_vendas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class DIM_Usuario(models.Model):
     nome = models.CharField(max_length=100)
+    estado = models.CharField(max_length= 50)
     
 
     def __str__(self):
