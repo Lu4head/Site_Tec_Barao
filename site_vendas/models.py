4095343c5ef85f9f49226b9f405a10ddbb156753
Luan <luan.emanuelriar@gmail.com>
2024-04-18 12:43:02 -0300

    Co-authored-by: GDuzzi <GDuzzi@users.noreply.github.com>

--- site_vendas/models.py
+++ site_vendas/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
-class usuario(models.Model):
+class DIM_Usuario(models.Model):
     nome = models.CharField(max_length=100)
     
 
     def __str__(self):
         return self.nome
+    
+class DIM_Produto(models.Model):
+
+    def __str__(self):
+        return self
+
+
+class DIM_Fornecedor(models.Model):
+
+    def __str__(self):
+        return self
+
+class FAT_venda(models.Model):
+
+    def __str__(self):
+        return self
