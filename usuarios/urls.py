a3bb7a04bea4e25792966f7fb227a91f922778ca
GDuzzi <gustavotduzzi@outlook.com>
2024-04-25 02:25:49 -0300

    Tela de inicio aparecendo que o usuario esta online logout funcionando tambem

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from usuarios import views
 
+
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('cadastro/', views.cadastro, name='cadastro')
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('logout/',  views.logout, name="logout")
 ]
