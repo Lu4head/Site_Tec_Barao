a8ea6757fd8cd8a8ea5ad8db6d62d048c4050403
Luan <luan.emanuelriar@gmail.com>
2024-04-27 21:55:17 -0300

    Forms por tipo

--- site_vendas/forms.py
+++ site_vendas/forms.py
@@ -2,7 +2,12 @@ from django import forms
 from .models import DIM_Fornecedor,DIM_Produto, FAT_item_nota
 from usuarios.models import DIM_Usuario
 
-class vestuarioForms(forms.Form):
+lista_tamanhos = [("P","P"),
+                ("M","M"),
+                ("G","G"),
+                ]
+class camisetaForms(forms.Form):
+
     quantidade = forms.IntegerField(
         label="Quantidade a comprar",
         required=True,
@@ -13,6 +18,24 @@ class vestuarioForms(forms.Form):
                 "class": "",
                 "id": "",
                 "placeholder": "1",
+            }
+        )
+    )
+    
+    tamanho = forms.ChoiceField(
+        label = "Tamanho:",
+        required = True,
+        choices = lista_tamanhos, # Possíveis escolhas importada do dicionário de cursos da Classe/Model DIM_Usuario definida em usuarios/models.py
+        widget = forms.Select(attrs={"class":"form-control"}) # Define campo de seleção de escolha
+    )
+
+    nome = forms.CharField(
+        label="Nome escrito na estampa",
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Insira o nome aqui",
             }
         )
     )
@@ -33,5 +56,99 @@ class canecaforms(forms.Form):
             }
         )
     )
+    nome = forms.CharField(
+        label="Nome escrito na caneca",
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Insira o nome aqui",
+            }
+        )
+    )
 
-    
+class blusaForms(forms.Form):
+    quantidade = forms.IntegerField(
+        label="Quantidade a comprar",
+        required=True,
+        max_value=99,
+        min_value=1,
+        widget=forms.NumberInput(
+            attrs={
+       
+         "class": "",
+                "id": "",
+                "placeholder": "1",
+            }
+        )
+    )
+
+    tamanho = forms.ChoiceField(
+        label = "Tamanho:",
+        required = True,
+        choices = lista_tamanhos, # Possíveis escolhas importada do dicionário de cursos da Classe/Model DIM_Usuario definida em usuarios/models.py
+        widget = forms.Select(attrs={"class":"form-control"}) # Define campo de seleção de escolha
+    )
+
+
+class tiranteForms(forms.Form):
+    quantidade = forms.IntegerField(
+        label="Quantidade a comprar",
+        required=True,
+        max_value=99,
+        min_value=1,
+        widget=forms.NumberInput(
+            attrs={
+       
+         "class": "",
+                "id": "",
+                "placeholder": "1",
+            }
+        )
+    )
+
+    nome = forms.CharField(
+        label="Nome escrito no tirante",
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Insira o nome aqui",
+            }
+        )
+    )
+
+    rede_social = forms.CharField(
+    label="@ de alguma rede social para o tirante\n*não obrigatório",
+    required= False,
+    max_length=30,
+    widget=forms.TextInput(
+        attrs={
+            "class":"form-control",
+            "placeholder":"Insira o @ aqui",
+            }
+        )
+    )
+
+class shortForms(forms.Form):
+    quantidade = forms.IntegerField(
+        label="Quantidade a comprar",
+        required=True,
+        max_value=99,
+        min_value=1,
+        widget=forms.NumberInput(
+            attrs={
+       
+         "class": "",
+                "id": "",
+                "placeholder": "1",
+            }
+        )
+    )
+
+    tamanho = forms.ChoiceField(
+        label = "Tamanho:",
+        required = True,
+        choices = lista_tamanhos, # Possíveis escolhas importada do dicionário de cursos da Classe/Model DIM_Usuario definida em usuarios/models.py
+        widget = forms.Select(attrs={"class":"form-control"}) # Define campo de seleção de escolha
+    )
--- site_vendas/views.py
+++ site_vendas/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from site_vendas.models import DIM_Produto
-from .forms import vestuarioForms, canecaforms
-
+from .forms import camisetaForms , canecaforms , tiranteForms , blusaForms , shortForms
 
 def index(request): # Define view para a página home
     produtos = DIM_Produto.objects.filter(Produto_ativo_PRODUTO = True) # Lista objetos da classse DIM_Produto no banco filtrando pelos objetos que tenham o parâmetro Produto_ativo = True
@@ -10,10 +9,18 @@ def index(request): # Define view para a página home
 
 def produto(request, nome): # Define view para a página de produto
     produto = get_object_or_404(DIM_Produto, Nome_PRODUTO=nome) # Ao selecionar um produto na página home o usuário será direcionado à página do produto selecionado filtando pela pk = produto_id definida no html
-    if produto.Tipo_PRODUTO == "caneca":
-        form = canecaforms()
-    if produto.Tipo_PRODUTO == "vestuario":
-        form = vestuarioForms(produto = produto)
+    form = 0
+    match produto.Tipo_PRODUTO:
+        case "camiseta":
+            form = camisetaForms()
+        case "caneca":
+            form = canecaforms()
+        case "blusa":
+            form = blusaForms()
+        case "tirante":
+            form = tiranteForms()
+        case "short":
+            form = shortForms()
     return render(request,'site_vendas/produto.html',{'produto': produto, "form": form })
 
 
