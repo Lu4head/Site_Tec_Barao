a57b6eaeb1df8262d724076e8abf6647a344c8b1
Luan <luan.emanuelriar@gmail.com>
2024-04-27 23:10:03 -0300

    view formulario produtos quase pronta

--- site_vendas/models.py
+++ site_vendas/models.py
@@ -53,10 +53,10 @@ class FAT_Nota(models.Model):
     Data_VENDA = models.DateField(verbose_name= "Data da venda",default=timezone.now)
 
 class FAT_item_nota (models.Model):
-    Nota_fiscal = models.ForeignKey(FAT_Nota, on_delete= models.CASCADE)
-    Id_USUARIO = models.ForeignKey(DIM_Usuario, on_delete= models.CASCADE, default='')
-    Id_PRODUTO = models.ForeignKey(DIM_Produto, on_delete=models.CASCADE)
-    Id_FORNECEDOR = models.ForeignKey(DIM_Fornecedor, on_delete=models.CASCADE)
+    Nota_fiscal = models.ForeignKey(FAT_Nota, blank=True, null = True, on_delete= models.CASCADE, default='')
+    Id_USUARIO = models.ForeignKey(DIM_Usuario, blank=True, null = True, on_delete= models.CASCADE, default='')
+    Id_PRODUTO = models.ForeignKey(DIM_Produto, blank=True, null = True, on_delete=models.CASCADE, default="")
+    Id_FORNECEDOR = models.ForeignKey(DIM_Fornecedor, blank=True, null = True, on_delete=models.CASCADE, default='')
     Qtd_item = models.IntegerField(null= False, verbose_name="Quantidade de venda",default=1)
     Valor_total_item = models.DecimalField(max_digits= 6, decimal_places=2, null= True,blank=False, verbose_name="Valor total por produto")
     Personalizacao_1 = models.CharField(max_length=30, null=True,blank= True)
--- site_vendas/views.py
+++ site_vendas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from site_vendas.models import DIM_Produto
+from site_vendas.models import DIM_Produto, FAT_item_nota
 from .forms import camisetaForms , canecaforms , tiranteForms , blusaForms , shortForms
 
 def index(request): # Define view para a página home
@@ -21,6 +21,37 @@ def produto(request, nome): # Define view para a página de produto
             form = tiranteForms()
         case "short":
             form = shortForms()
+
+    if request.method == 'POST':
+        match produto.Tipo_PRODUTO:
+            case "camiseta":
+                form = camisetaForms(request.POST)
+            case "caneca":
+                form = canecaforms(request.POST)
+            case "blusa":
+                form = blusaForms(request.POST)
+            case "tirante":
+                form = tiranteForms(request.POST)
+            case "short":
+                form = shortForms(request.POST)
+
+        if form.is_valid():
+            quantidade = form.cleaned_data.get('quantidade')
+            tamanho = form.cleaned_data.get('tamanho')
+            nome_estampa = form.cleaned_data.get('nome')
+            rede_social = form.cleaned_data.get('rede_social')
+        
+        fat_item_nota = FAT_item_nota.objects.create(
+            Nota_fiscal = None,
+            Id_USUARIO = None,
+            Id_PRODUTO = produto,
+            Id_FORNECEDOR = None,
+            Qtd_item = quantidade,
+            Valor_total_item = produto.Preco_produto * quantidade,
+            Personalizacao_1 = tamanho if tamanho else nome_estampa,
+            Personalizacao_2 = nome_estampa if (tamanho and nome_estampa) else rede_social,
+        )
+    
     return render(request,'site_vendas/produto.html',{'produto': produto, "form": form })
 
 
